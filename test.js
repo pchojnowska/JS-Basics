81302a6ac16b20201de440c5326a924f7d54a9b3
paulina <antaria@vp.pl>
2018-03-04 19:33:33 +0100

    JS for Beginners - Lesson 21

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function getAverage (a,b) {
-    var average = (a + b)/2;
+    var average = (a + b)/2; // a local variable
     console.log(average);
     return average;
 }
 
 getAverage(1,2);
-var myResult = getAverage(1,2);
+var myResult = getAverage(1,2); // a global variable
 console.log("the average of my numbers is " + myResult);
