6043a593587dfd3b92ef1a31a6f2796d4dd907e0
paulina <antaria@vp.pl>
2018-03-04 18:51:36 +0100

    JS for Beginners - Lesson 13

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-var myNum = 10;
+var myAge = 29;
 
-if (myNum > 10) {
-    document.write("myNum is greater than 10");
+if (myAge > 30) {
+
+    document.write("you are over 30!");
+
+} else if (myAge > 20) {
+
+    document.write("you are over 20!");
+
+} else if (myAge > 10) {
+
+    document.write("you are over 10!");
+
 } else {
-    document.write("myNum is not greater than 10");
+
+    document.write("you are not over 10!");
+
 }
