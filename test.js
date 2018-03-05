66a3a4e4d64eb7329e1796a433d6922210fcfb2b
paulina <antaria@vp.pl>
2018-03-05 21:32:28 +0100

    JS for Beginners - Lesson 36

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
 /* var myBody = document.getElementsByTagName("body");
 myBody[0].innerHTML = "<p>I'm a paragraph</p>"; */
 
-var firstLinkText = document.getElementById("first-link");
-firstLinkText.textContent = "My first super link";
+/* var firstLinkText = document.getElementById("first-link");
+firstLinkText.textContent = "My first super link"; */
+
+var link = document.getElementById("test");
+link.setAttribute("class", "toyota");
+link.setAttribute("alt", "This is a link");
+link.className = "ninja";
+
+
+var styledLink = document.getElementById("third-link");
+styledLink.setAttribute("style", "position: relative; left: 10px;");
+
+styledLink.style.margin = "10px";
+styledLink.style.color = "green";
+styledLink.style.backgroundColor = "yellow";
