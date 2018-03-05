3fb9f6e3effbbf9d5fa4e0f5488abb07f3fc5ff0
paulina <antaria@vp.pl>
2018-03-05 21:12:56 +0100

    JS for Beginners - Lesson 34 (DOM basics)

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-function getAverage (a,b) {
-    var average = (a + b)/2; // a local variable
-    console.log(average);
-    return average;
-}
+/* var myBody = document.getElementsByTagName("body");
+myBody[0].innerHTML = "<p>I'm a paragraph</p>"; */
 
-getAverage(1,2);
-var myResult = getAverage(1,2); // a global variable
-console.log("the average of my numbers is " + myResult);
+var firstLinkText = document.getElementById("first-link");
+firstLinkText.textContent = "My first super link";
