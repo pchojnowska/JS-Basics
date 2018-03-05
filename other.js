7b9d282679797b4924a377eb840d654bca7a1aa4
paulina <antaria@vp.pl>
2018-03-05 20:27:05 +0100

    JS for Beginners - Lesson 31

--- other.js
+++ other.js
@@ -1,3 +1,5 @@
+//constructor function
+
 var Car = function(maxSpeed, driver) {
     this.maxSpeed = maxSpeed;
     this.driver = driver;
@@ -16,3 +18,13 @@ myCar.drive(30,4);
 myCar2.logDriver();
 console.log(myCar.maxSpeed);
 
+// Date object
+var birthday = new Date(1980, 3, 1, 10, 20, 15);
+var birthday2 = new Date(1980, 3, 1, 10, 20, 15);
+console.log(birthday.getTime());
+
+if (birthday.getTime()===birthday2.getTime()) {
+    console.log("You were born on the same day");
+} else {
+    console.log("We have different dates of birth");
+}
