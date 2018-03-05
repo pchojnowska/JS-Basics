1aff3897988b633b18e00c578dc7ba6d5fc18188
paulina <antaria@vp.pl>
2018-03-05 22:03:24 +0100

    JS for Beginners - Lesson 38

--- test.js
+++ test.js
@@ -4,12 +4,13 @@ myBody[0].innerHTML = "<p>I'm a paragraph</p>"; */
 /* var firstLinkText = document.getElementById("first-link");
 firstLinkText.textContent = "My first super link"; */
 
+// Let's add attributes
 var link = document.getElementById("test");
 link.setAttribute("class", "toyota");
 link.setAttribute("alt", "This is a link");
 link.className = "ninja";
 
-
+// Let's add some styling to HTML elements
 var styledLink = document.getElementById("third-link");
 styledLink.setAttribute("style", "position: relative; left: 10px;");
 
@@ -17,8 +18,18 @@ styledLink.style.margin = "10px";
 styledLink.style.color = "green";
 styledLink.style.backgroundColor = "yellow";
 
+//Let's add a new list item before the JS script reference
 var newHref = document.createElement("a");
 var allLinks = document.getElementsByTagName("body")[0];
 allLinks.insertBefore(newHref, allLinks.getElementsByTagName("script")[0]);
 newHref.innerHTML = "New link";
 newHref.setAttribute("href", "#");
+
+// Let's remove the first link - it's Link zero anyways
+var toBeRemoved = allLinks.getElementsByTagName("a")[0];
+
+// store it in a variable to reinstall it back if needed
+var removed = allLinks.removeChild(toBeRemoved);
+
+// to add the removed element back
+// allLinks.appendChild(removed);
