223d1e6e7ba3159fb5bbda904c60b35f17f27b70
paulina <antaria@vp.pl>
2018-03-07 21:35:30 +0100

    JS for Beginners - Lesson 44 (forms)

--- test-form.js
+++ test-form.js
@@ -1,8 +1,24 @@
 var myForm = document.forms.myForm;
+var message = document.getElementById("message");
 myForm.name.onfocus = function() {
     myForm.name.style.border = "4px solid pink";
 };
 
 myForm.name.onblur = function() {
     myForm.name.style.border = "none";
+};
+
+
+//validation for name input
+
+myForm.onsubmit = function() {
+
+    if(myForm.name.value == "") {
+        message.innerHTML = "please enter a name";
+        return false;
+
+    } else{
+        message.innerHTML = "";
+        return true;
+    }
 };
