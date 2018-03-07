b9456bce32c97d7fc827298679a4a0c66da589eb
paulina <antaria@vp.pl>
2018-03-07 21:18:49 +0100

    JS for Beginners - Lesson 43 (forms)

---
+++ test-form.js
@@ -0,0 +1,8 @@
+var myForm = document.forms.myForm;
+myForm.name.onfocus = function() {
+    myForm.name.style.border = "4px solid pink";
+};
+
+myForm.name.onblur = function() {
+    myForm.name.style.border = "none";
+};
